665ceda2f3d6aebada2cd43b7b547c2f078b0de4
Sugam <Sugam11223@gmail.com>
2025-03-16 21:24:34 +0545

    Before clz

--- loyalty_system/loyalty_system/settings.py
+++ loyalty_system/loyalty_system/settings.py
@@ -85,7 +85,7 @@ DATABASES = {
     }
 }
 
-AUTH_USER_MODEL = 'myapp.Customer'
+#AUTH_USER_MODEL = 'users.User'
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
 
